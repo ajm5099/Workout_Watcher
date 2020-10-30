ad529368a000909bfb769d15bebaeb2943711022
Alex Milroy <amilroy@gmail.com>
2020-10-29 22:03:29 -0700

    working on routes

--- server.js
+++ server.js
@@ -21,25 +21,50 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", { u
 //WORKOUT ROUTES
 //=========================================================================
 
-//write the route to create a new workout
+//TODO:write the route to create a new workout
+//TODO: Be sure to modify this route to be based on user input
+db.Workout.create({ name: "Change this name to be user input" })
+.then(dbWorkout => {
+    console.log(dbWorkout);
+})
+.catch(({message}) => {
+    console.log(message);
+});
+//TODO:Write the route to get all workouts
+app.get("/workouts", (req, res) =>{
+    db.Workout.find({})
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.json(err);
+    });
+});
 
-//Write the route to get all workouts
+//TODO:Write the route to update the attributes of a specific workout
 
-//Write the route to update the attributes of a specific workout
-
-//Write the route to delete a specific workout
+//TODO:Write the route to delete a specific workout
 
 //=========================================================================
 //EXERCISE ROUTES
 //=========================================================================
 
-//write the route to create a new exercise
+//TODO:write the route to create a new exercise
 
-//Write the route to get all exercises in a workout
+//TODO:Write the route to get all exercises in a workout
+app.get("/exercises", (req, res) =>{
+    db.Exercise.find({})
+    .then(dbExercise => {
+        res.json(dbExercise);
+    })
+    .catch(err => {
+        res.json(err);
+    });
+});
 
-//Write the route to update the attributes of a specific exercise
+//TODO:Write the route to update the attributes of a specific exercise
 
-//Write the route to delete a specific exercise
+//TODO:Write the route to delete a specific exercise
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
