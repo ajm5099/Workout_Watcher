c7199812a1cf62ba7ed835ad14882a8d2c12c245
Alex Milroy <amilroy@gmail.com>
2020-10-29 20:34:04 -0700

    initial file setup

---
+++ models/exercise.js
@@ -0,0 +1,16 @@
+//=========================================================
+//Use this as a guide
+//=========================================================
+
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const BookSchema = new Schema({
+  author: String,
+  title: String
+});
+
+const Book = mongoose.model("Book", BookSchema);
+
+module.exports = Book;
